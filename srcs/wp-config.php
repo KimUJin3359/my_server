1d4eb230678f8d104310af7aefa05321b06cbbf1
KimUJin3359 <50474972+KimUJin3359@users.noreply.github.com>
2021-03-09 01:51:45 +0900

    Update wp-config.php

--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'ujin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '1q2w3e4r!' );
+define( 'DB_PASSWORD', 'This will be your password which you made' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         ',`G5[<L5;LyJ<DVosB!lpu+D9xf.7VbKOVkk_#^D|].V~N|B<7?MPuoM0N.Z|<O9');
-define('SECURE_AUTH_KEY',  '%ni38t=>P]eYvv,Q:u|%*&6y>!)Fpzodae6|l(.U-hgG|9k<M[+o4M0!u#nY/3dn');
-define('LOGGED_IN_KEY',    '5&R8c*X>,kyh[j>RHm`Ye!jGwpLg4Vv?L>TSZz*FWGuXn~DFmYcQE3)8V*R&]y:a');
-define('NONCE_KEY',        'vijzSPo;X%dOz80{D%~b|x(1wSgnpmmq^i@i7nlV<rI97?OA(MMq,=L-~xaMZg}q');
-define('AUTH_SALT',        '[wfk ^P$2(^S+)1uK7|+a1IvD2;8$@7-W.{I=O;T+uv;SUme%|vbd:i7VF1<udX/');
-define('SECURE_AUTH_SALT', ',+01|clt[h() yIB|H|Y[V|Po{-4iJOD(T,,Lkb:2_HuEl))a.J7Jq#G_R8uQr`O');
-define('LOGGED_IN_SALT',   'H(h{-MGC]3LQ*DLpbBr8F+3GSSlUQ[>Y-KVL`-ttCCPL:GkT&dU;9-,nKPj|{aX]');
-define('NONCE_SALT',       'V_ohuPR.Tr&<O<#pU`K]SG~IY1uYDf!j3Z`DGc-f/&`s3$.t;*ZDh[P&T<5FcHG ');
+define('AUTH_KEY',         'You');
+define('SECURE_AUTH_KEY',  'need');
+define('LOGGED_IN_KEY',    'to');
+define('NONCE_KEY',        'make');
+define('AUTH_SALT',        'your');
+define('SECURE_AUTH_SALT', 'own');
+define('LOGGED_IN_SALT',   'key');
+define('NONCE_SALT',       '.');
 
 /**#@-*/
 
